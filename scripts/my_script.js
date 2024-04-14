b0c9c902746e13c519da2a373446a93576ad73e8
Your Name <youremail@yourdomain.com>
2024-04-15 01:15:15 +0300

    "Updated HTML and JS for contact form; changed form element selection to use querySelector; added FormData usage; disabled submit button after click; prevent form submission if fields are empty"

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -1,18 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("contactForm").addEventListener("submit", function(event) {
+	document.querySelector(".contact__form").addEventListener("submit", function(event) {
 			event.preventDefault();
 
-			const name = document.getElementById("name").value;
-			const email = document.getElementById("email").value;
-			const message = document.getElementById("message").value;
-			
-			if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
-					alert("Please fill in all fields.");
-					return;
-			}
+			const formData = new FormData(this);
+			document.querySelector("button[type=submit]").innerText = "Спасибо!";
 
-			const submitButton = this.querySelector('button[type="submit"]');
-			submitButton.innerText = "Спасибо!";
-			submitButton.disabled = true;
 	});
 });
